b0a2e035f0ed602b449e7d33a0d75d2a5d054362
HanOon_32 <dr.omegakh32@gmail.com>
2026-06-05 12:05:48 +0000

    Fri, Jun 5, 2026, 12:05 PM +00:00

--- hani.py/Hanoon.py/Python training with Harvard University1/hello.py
+++ hani.py/Hanoon.py/Python training with Harvard University1/hello.py
@@ -290,10 +290,22 @@ else :
     print("score: f")
 
 
+# نفس الكود لكن بطريقة ابسط
+# Same code but in a simpler way
 
 
+score = int(input("Score: "))
 
-
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
 
 
 
