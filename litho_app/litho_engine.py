b90fdb8dfb7e092e02283ea575ce05f3dbf7c14d
HaniStudio <dr.omegakh32@gmail.com>
2026-07-03 15:33:23 +0300

    Delete litho_app directory

--- litho_app/litho_engine.py
+++
@@ -1,5 +0,0 @@
-from litho_planar import generate_planar_lithophane
-
-
-def generate_lithophane(**params):
-    return generate_planar_lithophane(**params)
--- litho_app/litho_sphere.py
+++
@@ -1,8 +0,0 @@
-import trimesh
-
-def sphere_lithophane(image, radius, shell_thickness):
-    outer = trimesh.creation.icosphere(subdivisions=4, radius=radius)
-    inner = trimesh.creation.icosphere(
-        subdivisions=4, radius=radius - shell_thickness
-    )
-    return outer.difference(inner)
